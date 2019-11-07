16fade403c274cbc439f4161d6f21d657be9d656
unknown <ihab.hamed2000@gmail.com>
2019-11-07 04:32:31 +0200

    orignal script

---
+++ ClassLibrary1/ClassLibrary1/exhelp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using excel=Microsoft.Office.Interop.Excel;
+
+namespace ClassLibrary1
+{
+    public class exhelp
+    {
+        public string ExcelSetup(int x, int y)
+        {
+            excel.Application Xapp = new excel.Application();
+            excel.Workbook Xworkbook = Xapp.Workbooks.Open("F:\\selen\\emails");
+            excel._Worksheet Xworksheet = Xworkbook.Sheets[1];
+            excel.Range Xrange = Xworksheet.UsedRange;
+            return Xrange.Cells[x][y].value2;
+        }
+    }
+}
---
+++ ClassLibrary1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using ClassLibrary1;
+
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class Taravian
+    {
+        IWebDriver chrome = new ChromeDriver(@"F:\selen");
+        exhelp excelvalue = new exhelp();
+
+        //[Test, Order(5)]
+        [Test]
+        public void signupT()
+        {
+            for (int i=1;i<1000;i++)
+            {
+                chrome.Url = "https://tournament2019.travian.com/";
+                //System.Threading.Thread.Sleep(1 * 1000);
+                chrome.FindElement(By.Name("email")).SendKeys(excelvalue.ExcelSetup(i, 1));
+                chrome.FindElement(By.Name("email")).SendKeys(Keys.Enter);
+                System.Threading.Thread.Sleep(1 * 1000);
+                chrome.FindElement(By.XPath("/html/body/div[5]/div[3]/a")).Click();
+                System.Threading.Thread.Sleep(1 * 1000);
+
+            }
+            
+        }
+    }
+}
